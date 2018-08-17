fd6391b0c2bebf9692529c641548e27971beba85
ms2sato <ms2sato@CIRCLEAROUNDnoMacBook-ea.local>
2018-08-18 08:45:37 +0900

    draftlogin

--- xpo.go
+++ xpo.go
@@ -14,12 +14,32 @@ import (
 	"google.golang.org/appengine/user"
 )
 
+// XUser struct
+type XUser struct {
+	ID    string `datastore:"-" goon:"id"`
+	Name  string
+	Email string
+
+	// 他のアカウントの関連付けも同様にする。各アカウントタイプでIDをユニークにするためにあえて抽象化しない
+	AppEngineUserKey *datastore.Key
+}
+
+// AppEngineUser struct
+type AppEngineUser struct {
+	ID      string         `datastore:"-" goon:"id"`
+	UserKey *datastore.Key // parent を付けないのはログイン時、単独で本structを検索させるため
+	Email   string
+}
+
 // Report struct
 type Report struct {
 	ID        string         `datastore:"-" goon:"id"`
 	AuthorKey *datastore.Key `datastore:"-" goon:"parent"`
 	Author    string
 	Content   string
+	Year      int16
+	Month     int8
+	Day       int8
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
@@ -27,13 +47,21 @@ type Report struct {
 func init() {
 	http.HandleFunc("/", handleRoot)
 	http.HandleFunc("/reports", handleReports)
+	http.HandleFunc("/loggedin", handleLoggedIn)
 
 	// http.HandleFunc("/my", handleMy)
-	// http.HandleFunc("/loggedin", handleLoggedIn)
 }
 
 func userKey(c context.Context, email string) *datastore.Key {
 	return datastore.NewKey(c, "User", email, 0, nil)
+}
+
+func xUserKey(c context.Context, ID string) *datastore.Key {
+	return datastore.NewKey(c, "XUser", ID, 0, nil)
+}
+
+func appengineUserKey(c context.Context, ID string) *datastore.Key {
+	return datastore.NewKey(c, "AppEngineUser", ID, 0, nil)
 }
 
 func redirectUnlessLoggedIn(w http.ResponseWriter, r *http.Request) bool {
@@ -49,7 +77,6 @@ func redirectUnlessLoggedIn(w http.ResponseWriter, r *http.Request) bool {
 }
 
 func handleRoot(w http.ResponseWriter, r *http.Request) {
-	// ログインしてなければリダイレクト
 	if !redirectUnlessLoggedIn(w, r) {
 		return
 	}
@@ -91,16 +118,12 @@ func handleRoot(w http.ResponseWriter, r *http.Request) {
 }
 
 func handleReports(w http.ResponseWriter, r *http.Request) {
-	c := appengine.NewContext(r)
-	u := user.Current(c)
-
-	// ログインしてなければリダイレクト
-	if u == nil {
-		url, _ := user.LoginURL(c, "/")
-		http.Redirect(w, r, url, http.StatusFound)
+	if !redirectUnlessLoggedIn(w, r) {
 		return
 	}
 
+	c := appengine.NewContext(r)
+	u := user.Current(c)
 	g := goon.NewGoon(r)
 
 	id, ierr := uuid.NewRandom()
@@ -127,4 +150,51 @@ func handleReports(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	http.Redirect(w, r, "/", http.StatusFound)
+}
+
+func handleLoggedIn(w http.ResponseWriter, r *http.Request) {
+	if !redirectUnlessLoggedIn(w, r) {
+		return
+	}
+
+	c := appengine.NewContext(r)
+	u := user.Current(c)
+	g := goon.NewGoon(r)
+
+	aeu := &AppEngineUser{ID: u.ID}
+	if err := g.Get(aeu); err != nil {
+		if err != datastore.ErrNoSuchEntity {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		xu := &XUser{Name: u.ID, Email: u.Email}
+		xkey, ierr := g.Put(xu)
+		if ierr != nil {
+			http.Error(w, ierr.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		aeu.Email = u.Email
+		aeu.UserKey = xkey
+		akey, ierr := g.Put(aeu)
+		if ierr != nil {
+			http.Error(w, ierr.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		xu.AppEngineUserKey = akey
+		xkey, ierr = g.Put(xu)
+		if ierr != nil {
+			http.Error(w, ierr.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+
+	q := datastore.NewQuery("AppEngineUser").Order("-CreatedAt").Limit(10)
+	reports := make([]Report, 0, 10)
+	if _, err := g.GetAll(q, &reports); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
