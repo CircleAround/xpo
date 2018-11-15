da2d0934a9bab550e475b55711daa8f7f8a56c84
ms2sato <ms2.sato@gmail.com>
2018-11-15 16:35:25 +0900

    project entity

---
+++ server/src/local/xpo/entities/project.go
@@ -0,0 +1,18 @@
+package entities
+
+import (
+	"time"
+
+	"google.golang.org/appengine/datastore"
+)
+
+// Project struct
+type Project struct {
+	ID            int64          `json:"id" datastore:"-" goon:"id"`
+	OwnerKey      *datastore.Key `json:"-" datastore:"ownerKey" validate:"required"`
+	OwnerID       string         `json:"ownerId" validate:"required"`
+	Name          string         `json:"name" validate:"required"`
+	RepositoryURL string         `json:"repositoryUrl" validate:"url"`
+	CreatedAt     time.Time      `json:"createdAt"`
+	UpdatedAt     time.Time      `json:"updatedAt"`
+}
